989eaf2944578c0ebfb3dcc6144ae464aef6b3dd
Vladislav <git13vl@gmail.com>
2022-06-16 22:44:58 +0200

    Initial Commit

---
+++ Shared/Multiplatform_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplatform_appApp.swift
+//  Shared
+//
+//  Created by Vladislav Glovatskiy on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Multiplatform_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
